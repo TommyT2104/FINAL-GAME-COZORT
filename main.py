cb81030993fba89f2d7ddf6fa88925dc5c1b92cf
TommyT2104 <126900908+TommyT2104@users.noreply.github.com>
2023-05-10 12:03:20 -0700

    Added final changes to my code

--- main.py
+++ main.py
@@ -1,63 +1,49 @@
 # File Created by Thomas Trombatore
 # works cited: https://www.youtube.com/watch?v=Nt7WJa2iu0s
 # https://www.youtube.com/watch?v=QhD015WUMxE
+# https://www.codecademy.com/resources/blog/web-scraping-python-beautiful-soup-mlb-stats/
 
 # imports library requests
 import requests
 from bs4 import BeautifulSoup 
+import pandas as pd
 
-# pulls standings/stats from website
+# pulls standings/stats from websites
 url = 'https://www.baseball-reference.com/leagues/majors/2023.shtml'
 response = requests.get(url)
 
-# adds in html parser which assists with webscrapin
+# adds in html parser which assists with webscraping
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
+# defines the name of each team
+Name = soup.findAll("th", attrs={"data-stat":"team_name"})
 # defines the hit stats that are pulled from the website
 Hits = soup.findAll("td", attrs={"data-stat":"H"})
-
 # Defines the at bats and what part of the website is at bats
 AtBats = soup.findAll("td", attrs={"data-stat":"AB"})
-
 
 # Defines the batting average and what part of the website is batting average
 BattingAverage = soup.findAll("td", attrs={"data-stat":"batting_avg"})
 
-
 # a loop that will print the stats of the certain categories from the website
-for i in range(len(Hits)):
-    print(Hits[i].text + " Hits " + AtBats[i].text + " AtBats " + BattingAverage[i].text 
-          + " BattingAverage ") 
-
-# loop through the hits and at-bats data to calculate the predicted batting average for each player  
-for i in range(len(Hits)):
-    hits = int(Hits[i].text)
-    at_bats = int(AtBats[i].text)
-    if at_bats > 0:
-        predicted_batting_average = hits / at_bats
-        print(f"Predicted batting average for player {i+1}: {predicted_batting_average:.3f}")
-    else:
-        print(f"No at-bats for player {i+1}")
-
-
-
-# Initializes the variables to keep track of the index of the player and the player batting average
+for i in range(len(Hits)-1):
+    print(Name[i].text + " " + Hits[i].text + " Hits " + AtBats[i].text + " AtBats " 
+          + BattingAverage[i].text + " BattingAverage ") 
+    
+# initializes variables to keep track of batting average
 highest_batting_average = 0
 highest_batting_average_index = 0
 
-# create a list to store the names of the players
-player_names = [name.text for name in Names]
-
-# loop through the hits and at-bats data to calculate the predicted batting average for each player
+# loops through data needed from the website to predict the average for each team
 for i in range(len(Hits)):
     hits = int(Hits[i].text)
     at_bats = int(AtBats[i].text)
     if at_bats > 0:
         predicted_batting_average = hits / at_bats
-        if predicted_batting_average > highest_batting_average:
-            highest_batting_average = predicted_batting_average
-            highest_batting_average_index = i
+        highest_batting_average = predicted_batting_average 
+        highest_batting_average_index = i
 
-# print out the name of the player with the highest predicted batting average
-print(f"{player_names[highest_batting_average_index]} has the highest predicted batting average of {highest_batting_average:.3f}")
+# prints the team with the predicted highest batting average in the MLB
+print(f"{Name[highest_batting_average_index].text} has the highest predicted batting average in the MLB of {highest_batting_average:.3f}")
+
